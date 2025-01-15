dd39800b7a10fbe874ee6872ff35fff8cd5f182d
Justin <145566709+JS55CT@users.noreply.github.com>
2025-01-15 13:48:20 -0500

    Update GeoGPXer.js

--- GeoGPXer.js
+++ GeoGPXer.js
@@ -1,4 +1,5 @@
 /**
+ * ## Project Home < https://github.com/JS55CT/GeoGPXer >
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
